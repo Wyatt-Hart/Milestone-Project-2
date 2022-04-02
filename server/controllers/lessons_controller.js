65756f2a8a477345f1bfe0b7fe66114e9c576b30
HarakaHeartbroken <christophervfoster@gmail.com>
2022-04-01 23:20:35 -0700

    Stub of Delete Route re-added

--- server/controllers/lessons_controller.js
+++ server/controllers/lessons_controller.js
@@ -49,4 +49,18 @@ router.get('/data/seed', (req, res) => {
       })
   })
 
+  router.delete('/lessons/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/lessons')
+    }
+  })
+
 module.exports = router
